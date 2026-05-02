0f0e7fe47f1cc869b41007ed4434815362f8c3f9
Iksha Malhotra <iksha16005@gmail.com>
2026-05-02 13:15:16 +0530

    cleaned code

--- vehicle_maintenance_scheduler/server.js
+++ vehicle_maintenance_scheduler/server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const fetchGivenData = require("./api");
 const scheduleTasks = require("./vehiclescheduler");
 const Log = require("../logging_middleware/logger");
 
 const app = express();
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJpbTE3MzdAc3JtaXN0LmVkdS5pbiIsImV4cCI6MTc3NzcwNDE0OSwiaWF0IjoxNzc3NzAzMjQ5LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiOWIyNGE3ZTQtMWMzYS00YmYwLWI3YzMtMDE4ZGZmMDQzMTI0IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoiaWtzaGEgbWFsaG90cmEiLCJzdWIiOiI0NWZjYzNhMi0yZDhjLTQzYWYtYjEzNS0wNjhlNjhmNDNiODYifSwiZW1haWwiOiJpbTE3MzdAc3JtaXN0LmVkdS5pbiIsIm5hbWUiOiJpa3NoYSBtYWxob3RyYSIsInJvbGxObyI6InJhMjMxMTAwMzAzMDI0MiIsImFjY2Vzc0NvZGUiOiJRa2JweEgiLCJjbGllbnRJRCI6IjQ1ZmNjM2EyLTJkOGMtNDNhZi1iMTM1LTA2OGU2OGY0M2I4NiIsImNsaWVudFNlY3JldCI6IkpHeVBLVmhzRkRZc1dKY2sifQ.acQo_wcX4eeUC-r0_zt9NUFViUNgF_OJyNBfsDY5G5w";
+const token = process.env.TOKEN;
 
 const safeLog = async (...args) => {
   try {
@@ -46,6 +47,4 @@ app.get("/vehicleschedule", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("server running on 3000");
-});
+app.listen(3000);
